eda6f31601d97d633b99ae5bb708de35706c46e8
Jokiva <Jokiva@outlook.com>
2018-08-08 15:41:23 -0400

    indegree and outdegree counting methods added

--- Graph/lib/Network.hpp
+++ Graph/lib/Network.hpp
@@ -14,10 +14,22 @@ public:
 	~Network() {
 
 	}
+
+	virtual vector<size_t> get_indegree() const = 0;
+
+	virtual size_t get_indegree(size_t end) const = 0;
+
+	virtual vector<size_t> get_outdegree() const = 0;
+
+	virtual size_t get_outdegree(size_t out) const = 0;
+
 };
 
 
 class Path {
+	friend class Vertix;
+	friend class Network_Vertix;
+
 private:
 	size_t next;
 	int weight;
@@ -31,6 +43,8 @@ public:
 
 
 class Vertix {
+	friend class Network_Vertix;
+
 private:
 	size_t index;
 	list<Path*> adj_list;
@@ -86,6 +100,32 @@ public:
 		}
 
 		return true;
+	}
+
+	vector<size_t> get_indegree() const {
+		vector<size_t> indegrees(vertix_num);
+
+		for (const Vertix* curr_vertix : vertices) {
+			for (const Path* curr_path : curr_vertix->adj_list) {
+				++indegrees[curr_path->next];
+			}
+		}
+	}
+
+	size_t get_indegree(size_t end) const {
+		return get_indegree()[end];
+	}
+
+	vector<size_t> get_outdegree() const {
+		vector<size_t> outdegrees(vertix_num);
+
+		for (size_t i = 0; i < vertix_num; ++i) {
+			outdegrees[i] = vertices[i]->adj_list.size();
+		}
+	}
+
+	size_t get_outdegree(size_t start) const {
+		return vertices[start]->adj_list.size();
 	}
 
 private:
